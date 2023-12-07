dd83f616baf9d3050d200722f0d93b42d7d7506c
VadimSorokolit <macintosh@ukr.net>
2023-12-07 19:33:49 +0200

    Project in progress........

--- LoginScreen/LoginScreen/ViewController.swift
+++ LoginScreen/LoginScreen/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBOutlet weak var userAccountTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var informationLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
