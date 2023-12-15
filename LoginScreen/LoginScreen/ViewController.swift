f15851f8d58919539e363f285a71249ce3687318
VadimSorokolit <macintosh@ukr.net>
2023-12-15 22:39:31 +0200

    Remade

--- LoginScreen/LoginScreen/ViewController.swift
+++ LoginScreen/LoginScreen/ViewController.swift
@@ -8,23 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
     @IBOutlet weak private var infoLabel: UILabel!
     @IBOutlet weak private var userAccountTextField: UITextField!
     @IBOutlet weak private var passwordTextField: UITextField!
     @IBOutlet weak private var logInButton: UIButton!
     @IBOutlet weak private var informationLabel: UILabel!
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapFunction))
+        self.setupLabels()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupLabels() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapFunction))
         self.informationLabel.isUserInteractionEnabled = true
         self.informationLabel.addGestureRecognizer(tap)
     }
     
+    // MARK: - IBActions
+    
     @IBAction func tapFunction(sender: UITapGestureRecognizer) {
         print("Task is done")
     }
+    
 }
 
