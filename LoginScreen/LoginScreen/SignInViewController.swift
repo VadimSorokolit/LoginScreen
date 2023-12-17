24aedacdc48a2c61d3ca0f664ee2b5edb5063a67
VadimSorokolit <macintosh@ukr.net>
2023-12-17 20:24:30 +0200

    no message

--- LoginScreen/LoginScreen/SignInViewController.swift
+++ LoginScreen/LoginScreen/SignInViewController.swift
@@ -37,10 +37,14 @@ class SignInViewController: UIViewController {
             self.infoLabelBottomConstraint.constant /= 2
         }
         
-//        userAccountTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: userAccountTextField.frame.height))
-//        userAccountTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: userAccountTextField.frame.height))
-//        userAccountTextField.leftViewMode = .always
-//        userAccountTextField.rightViewMode = .always
+    /*
+        
+        userAccountTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: userAccountTextField.frame.height))
+        userAccountTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: userAccountTextField.frame.height))
+        userAccountTextField.leftViewMode = .always
+        userAccountTextField.rightViewMode = .always
+         
+    */
          
        
     }
