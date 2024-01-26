3818d68c9e60835b3dc6676535435ca6a148c05e
VadimSorokolit <macintosh@ukr.net>
2024-01-26 21:32:37 +0200

    Fix logIn - logOut

--- LoginScreen/LoginScreen/NavigationController.swift
+++ LoginScreen/LoginScreen/NavigationController.swift
@@ -10,22 +10,44 @@ import FirebaseAuth
 
 class NavigationController: UINavigationController {
     
+    // MARK: Objects
+    
+    private struct LocalConstants {
+        static let isLoggedIn = "isLoggedIn"
+        static let userName = "userName"
+    }
+    
+    // MARK: Properties
+    
+    private let isLoggedIn = UserDefaults.standard.bool(forKey: LocalConstants.isLoggedIn)
+    private let userName = UserDefaults.standard.string(forKey: LocalConstants.userName)
+    
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Start auth_listener
-        Auth.auth().addStateDidChangeListener({ (auth: Auth, user: User?) -> Void in
-            if let user, let userName = user.email {
+        
+        if isLoggedIn {
+            if let userName {
                 if let homePageVC = self.storyboard?.instantiateViewController(withIdentifier: GlobalConstants.homePageViewControllerId) as? HomePageViewController {
                     homePageVC.userName = userName
                     self.setViewControllers([homePageVC], animated: false)
                 }
+            }
+        } else {
+            if let logInVC = self.storyboard?.instantiateViewController(withIdentifier: GlobalConstants.loginViewControllerId) as? LogInViewController {
+                self.setViewControllers([logInVC],animated: false)
+            }
+        }
+        
+        // Auth_listener
+        Auth.auth().addStateDidChangeListener({ (auth: Auth, user: User?) -> Void in
+            if let user, let userName = user.email {
+                UserDefaults.standard.setValue(true, forKey: LocalConstants.isLoggedIn)
+                UserDefaults.standard.setValue(userName, forKey: LocalConstants.userName)
             } else {
-                if let logInVC = self.storyboard?.instantiateViewController(withIdentifier: GlobalConstants.loginViewControllerId) as? LogInViewController {
-                    self.setViewControllers([logInVC],animated: false)
-                }
+                UserDefaults.standard.setValue(false, forKey: LocalConstants.isLoggedIn)
+                UserDefaults.standard.setValue(nil, forKey: LocalConstants.userName)
             }
         })
     }
